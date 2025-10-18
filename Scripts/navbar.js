727af18e4e37676a51028c6b10b73fac8bbef8c1
Eliott <tan.eliott04@gmail.com>
2025-10-18 22:32:34 +0200

    adding navbar

---
+++ Scripts/navbar.js
@@ -0,0 +1,4 @@
+fetch("Components/navbar.html")
+.then(Response => Response.text())
+.then(data => {document.getElementById("navbar").innerHTML = data;})
+.catch(error => Console.error("navbar loading error : ", error));
