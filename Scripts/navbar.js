908d007d2e975f17d4b67fa1e9397f7031a6d55b
Eliott <tan.eliott04@gmail.com>
2025-10-20 12:55:11 +0200

    adding truth page

--- Scripts/navbar.js
+++ Scripts/navbar.js
@@ -1,4 +1,4 @@
-fetch("Components/navbar.html")
+fetch("http:/Components/navbar.html")
 .then(Response => Response.text())
 .then(data => {document.getElementById("navbar").innerHTML = data;})
 .catch(error => Console.error("navbar loading error : ", error));
