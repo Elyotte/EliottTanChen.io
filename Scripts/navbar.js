612ce1e696ec537bee917618bf3cab0821300755
Eliott <tan.eliott04@gmail.com>
2025-10-21 22:50:03 +0200

    attempt on fixing navbar

--- Scripts/navbar.js
+++ Scripts/navbar.js
@@ -1,4 +1,10 @@
-fetch("http:/Components/navbar.html")
-.then(Response => Response.text())
-.then(data => {document.getElementById("navbar").innerHTML = data;})
-.catch(error => Console.error("navbar loading error : ", error));
+const base = window.location.pathname.includes("EliottTanChen.io")
+  ? "/EliottTanChen.io/"
+  : "./";
+
+fetch(`${base}Components/navbar.html`)
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("navbar").innerHTML = data;
+  })
+  .catch(error => console.error("Navbar loading error:", error));
